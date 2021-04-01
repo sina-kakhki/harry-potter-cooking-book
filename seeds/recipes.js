38b8fe1c3ddfa3eb199e37e661bb81c2f642d628
johannbesas <johannbesas@gmail.com>
2021-04-02 11:46:05 +1300

    db functions in progress

--- seeds/recipes.js
+++ seeds/recipes.js
@@ -13,7 +13,7 @@ exports.seed = function(knex) {
         {id: 6, name: 'Butterbeer Sugar Cookies', description: 'Lightly butterscotchy, with vanilla and brown sugar accents, just like the Wizarding World drink!', tags: ''},
         {id: 7, name: 'Calzone Snake', description: 'Whether youre a Slytherin fan, or just want to recreate the slaying of Nagini by Neville Longbottom, this calzone snake is sure to be the star of your spread', tags: ''},
         {id: 8, name: 'Cockroach Clusters', description: 'A Honeydukes specialty and Weasley boys favorite, chocolatey cockroach clusters are bite-sized treats that are perfect for tricking friends into eating bugs', tags: ''},
-        {id: 9, name: 'Adult Butterbeer', description: 'dAlthough the butterbeer in the books isnt alcoholic, that shouldnt stop you from splashing a little booze in your next batch', tags: ''},
+        {id: 9, name: 'Adult Butterbeer', description: 'Although the butterbeer in the books isnt alcoholic, that shouldnt stop you from splashing a little booze in your next batch', tags: ''},
         {id: 10, name: 'Harrys Birthday Cake', description: 'Recreate that iconic dessert with a batch of sweet strawberry cake', tags: ''},
         
         ]);
