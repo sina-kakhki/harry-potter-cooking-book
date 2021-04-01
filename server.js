7a24a52b3b5d0d7af16cb6b1814b7c63443e3056
Johnny <jvsainsbury@gmail.com>
2021-04-02 10:28:58 +1300

    set up recipe routes

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const server = express()
+const recipeRoutes = require('./recipeRoutes')
+
 
 // Server configuration
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
+// Routes config - change here to match the hbs files
+server.use('/recipe', recipeRoutes)
+
 
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
