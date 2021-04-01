cdfbd59f8c1c64dc2b502318b4a283acafe9c439
Johnny <jvsainsbury@gmail.com>
2021-04-01 20:06:32 +1300

    install scripts to packagae.json

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+module.exports = server
