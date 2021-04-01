74dd0a8593e67306a895c4a64eb8fcd536bb1515
1Karol1 <kbajczyk@gmail.com>
2021-04-02 11:28:17 +1300

    adding steps

--- seeds/details.js
+++ seeds/details.js
@@ -5,9 +5,16 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, recipe_id: 1, prepTime: 20, additionalTime: 15, servings: 10, yield: 20,}, 
+        {id: 2, recipe_id: 2, prepTime: 25, additionalTime: 10, servings: 4, yield: 8,},
+        {id: 3, recipe_id: 3, prepTime: 40, additionalTime: 15, servings: 4, yield: 4,},
+        {id: 4, recipe_id: 4, prepTime: 35, additionalTime: 20, servings: 8, yield: 10,},
+        {id: 5, recipe_id: 5, prepTime: 25, additionalTime: 10, servings: 6, yield: 8},
+        {id: 6, recipe_id: 6, prepTime: 60, additionalTime: 30, servings: 6, yield: 8,},
+        {id: 7, recipe_id: 7, prepTime: 40, additionalTime: 10, servings: 3, yield: 5,},
+        {id: 8, recipe_id: 8, prepTime: 20, additionalTime: 5, servings: 4, yield: 4,},
+        {id: 9, recipe_id: 9, prepTime: 40, additionalTime: 15, servings: 12, yield: 20,},
+        {id: 10, recipe_id: 10, prepTime: 60, additionalTime: 40, servings: 8, yield: 8,},
       ]);
     });
 };
