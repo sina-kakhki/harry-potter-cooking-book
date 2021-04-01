53f88215c39ea17269754a03da803b2aba3d0e9e
johannbesas <johannbesas@gmail.com>
2021-04-01 20:25:19 +1300

    steps migration completed

---
+++ migrations/20210401072426_steps.js
@@ -0,0 +1,11 @@
+exports.up = (knex) => {
+    return knex.schema.createTable('steps', (table) => {
+      table.increments('id').primary()
+      table.integer('recipe_id').references('recipe.id')
+      table.string('step')
+    })
+  }
+  
+  exports.down = (knex) => {
+    return knex.schema.dropTable('steps')
+  }
