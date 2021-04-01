b2806577ddc04d70f7d4fd8059f3d0c615b851b2
johannbesas <johannbesas@gmail.com>
2021-04-02 10:08:11 +1300

    set up seeds

---
+++ seeds/recipes.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipes').insert([
+        {id: 1, name: 'Butterbeer II', description: 'desc', tags: ''},
+        {id: 2, name: 'rowValue1', description: 'desc', tags: ''},
+        {id: 3, name: 'rowValue1', description: 'desc', tags: ''}
+      ]);
+    });
+};
